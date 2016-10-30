d0195ab6879598db8a4225a1e46cea92064cfa3c
Tim <tim.bourgault@gmail.com>
2016-10-30 14:49:07 -0700

    fix error in inquiry.js route.

--- app/routes/inquiry.js
+++ app/routes/inquiry.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return.this.store.findRecord('inquiry', params.inquiry_id);
+    return this.store.findRecord('inquiry', params.inquiry_id);
   },
   actions: {
     update(inquiry, params) {
