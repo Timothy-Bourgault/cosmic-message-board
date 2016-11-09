5160f7d809f8413d9b9bee94ad508080a13f53e2
Tim <tim.bourgault@gmail.com>
2016-11-08 23:12:29 -0800

    include a computed property that puts the antwoord authors first and last name together.

--- app/components/antwoord-tile.js
+++ app/components/antwoord-tile.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  antwoordAuthor: Ember.computed('antwoord.firstname', 'antwoord.lastname', function(){
+    return this.get('antwoord.firstname') + ' ' + this.get('antwoord.lastname');
+  })
 });
